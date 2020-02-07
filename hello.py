4b266c0dc48cfb2b54f3dd688df1968c6c231d38
JulesUE4 <37224790+julesue4@users.noreply.github.com>
2020-02-08 00:40:22 +0100

    first test with dice

--- hello.py
+++ hello.py
@@ -2,12 +2,13 @@ from random import randint
 
 print("Hello world !")
 
-print ("What's the name of the first player ?")
+print("What's the name of the first player ?")
 NamePlayer = input()
 de = range(1, 6)
 firstDice = randint(1, 6)
 secDice = randint(1, 6)
 thirdDice = randint(1, 6)
 
-print(NamePlayer, ' Launch the dices and make a', firstDice, ' a ', secDice, ' and a ', thirdDice)
+print(NamePlayer, " Launch the dices and make a", firstDice, " a ", secDice, " and a ", thirdDice)
+
 
